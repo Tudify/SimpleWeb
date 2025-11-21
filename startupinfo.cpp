a7624cf49b3f3c40b5370c190e533773397a298f
tudify <support@tudify.co.uk>
2025-11-21 18:03:45 +0000

    Add files via upload

---
+++ startupinfo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::string EngineVer = "3.0.5";
+  std::cout << "Engine version: " << EngineVer << " (does not reflect SimpleWeb version)" << std::endl;
+  return 0;
+}
